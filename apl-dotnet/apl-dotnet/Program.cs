b8b76607561ec217a36d7d0502301c6f46a973a7
Luca Fabbri <l.fabbri@rch.it>
2022-11-24 17:34:52 +0100

    WIP

--- apl-dotnet/apl-dotnet/Program.cs
+++ apl-dotnet/apl-dotnet/Program.cs
@@ -1,15 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
 using Antlr4.Runtime;
-var value = "(x=3 and y>10) or (x in [3,4,5] and y in [6,7,8])";
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+
+//var value = "(x=3 and y>10) or (x in [3,4,5] and y in [6,7,8])";
+var value = "x=3 ";
 AntlrInputStream inputStream = new AntlrInputStream(value);
-APLGrammarLexer aplGrammarLexer = new APLGrammarLexer(inputStream);
-CommonTokenStream commonTokenStream = new CommonTokenStream(aplGrammarLexer);
-APLGrammarParser aplGrammarParser = new APLGrammarParser(commonTokenStream);
-APLGrammarParser.ClauseContext clauseContext = aplGrammarParser.clause();
-APLVisitor visitor = new APLVisitor();
-var visit = visitor.Visit(clauseContext);
+APLGrammarLexer lexer = new APLGrammarLexer(inputStream);
+CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+APLGrammarParser parser = new APLGrammarParser(tokenStream);
 
-Console.WriteLine(visit.ToString());
+var tree = parser.clause();
 
-public class APLVisitor : APLGrammarBaseVisitor<object>
-{}
+
+Console.ReadLine();
+
+public class ClauseVisitor<TEntity> : APLGrammarBaseVisitor<IQueryable<TEntity>> where TEntity : class 
+{
+    private readonly IQueryable<TEntity> Items;
+
+    public ClauseVisitor(IQueryable<TEntity> items)
+    {
+        Items = items;
+    }
+
+    public override IQueryable<TEntity> VisitClause([NotNull] APLGrammarParser.ClauseContext context)
+    {
+        if(context.conjunction() != null)
+        {
+        }
+        return new ClauseExpression();
+    }
+}
+
+public class StringOperationVisitor<TEntity> : APLGrammarBaseVisitor<IQueryable<TEntity>> where TEntity : class
+{
+    private readonly IQueryable<TEntity> Items;
+
+    public StringOperationVisitor(IQueryable<TEntity> items)
+    {
+        Items = items;
+    }
+}
+
+public class NumberOperationVisitor<TEntity> : APLGrammarBaseVisitor<IQueryable<TEntity>> where TEntity : class
+{
+    private readonly IQueryable<TEntity> Items;
+
+    public NumberOperationVisitor(IQueryable<TEntity> items)
+    {
+        Items = items;
+    }
+
+}
+
+public class BoolOperationVisitor<TEntity> : APLGrammarBaseVisitor<IQueryable<TEntity>> where TEntity : class
+{
+    private readonly IQueryable<TEntity> Items;
+
+    public BoolOperationVisitor(IQueryable<TEntity> items)
+    {
+        Items = items;
+    }
+
+}
