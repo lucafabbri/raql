f9686ebbae009ee4bcee3da2b4ff9fdf9c928e50
lfabbri <l.fabbri@rch-group.com>
2022-11-24 21:48:56 +0100

    WIP

--- apl-dotnet/apl-dotnet/Program.cs
+++ apl-dotnet/apl-dotnet/Program.cs
@@ -1,82 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Linq.Expressions;
+using System.Linq;
 using System.Reflection;
 using Antlr4.Runtime;
-using Antlr4.Runtime.Misc;
 using Antlr4.Runtime.Tree;
+using Higrow.AQL.Linq;
 
 //var value = "(x=3 and y>10) or (x in [3,4,5] and y in [6,7,8])";
-var value = "x=3 ";
-AntlrInputStream inputStream = new AntlrInputStream(value);
-APLGrammarLexer lexer = new APLGrammarLexer(inputStream);
-CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-APLGrammarParser parser = new APLGrammarParser(tokenStream);
+var query = "x in [1,2]";
 
-var tree = parser.clause();
+IEnumerable<Vector> vectors = new List<Vector>()
+    {
+        new Vector(1,2),
+        new Vector(2,1),
+        new Vector(3,1),
+        new Vector(3,2),
+        new Vector(2,4),
+        new Vector(6,3)
+    };
+
+foreach (var vector in vectors.AsQueryable().Aql(query).ToList())
+{
+    Console.WriteLine($"[{vector.x},{vector.y}]");
+}
 
 
 Console.ReadLine();
 
-public class ClauseVisitor<TEntity> : APLGrammarBaseVisitor<Expression<Func<TEntity, bool>>> where TEntity : class 
+class Vector
 {
-    private readonly Expression<Func<TEntity, bool>> Predicate;
+    public int x { get; set; }
+    public int y { get; set; }
 
-    public ClauseVisitor()
+    public Vector(int x, int y)
     {
-        Predicate = PredicateBuilder.False<>;
+        this.x = x;
+        this.y = y;
     }
-
-    public override Expression<Func<TEntity, bool>> VisitClause([NotNull] APLGrammarParser.ClauseContext context)
-    {
-        if(context.conjunction() != null)
-        {
-            var clauses = context.clause();
-            if(clauses != null)
-            {
-                switch (clauses.Length)
-                {
-                    case 2:
-                        var clauseVisitor1 = new ClauseVisitor<TEntity>(Predicate).Visit(clauses.First());
-                        return clauses[0].Vis
-                    case 1:
-                    case 0:
-                    default:
-                        break;
-
-                }
-            }
-        }
-    }
-}
-
-public class StringOperationVisitor<TEntity> : APLGrammarBaseVisitor<Expression<Func<TEntity, bool>>> where TEntity : class
-{
-    private readonly Expression<Func<TEntity, bool>> Predicate;
-
-    public StringOperationVisitor(Expression<Func<TEntity, bool>> predicate)
-    {
-        Predicate = predicate;
-    }
-}
-
-public class NumberOperationVisitor<TEntity> : APLGrammarBaseVisitor<Expression<Func<TEntity, bool>>> where TEntity : class
-{
-    private readonly Expression<Func<TEntity, bool>> Predicate;
-
-    public NumberOperationVisitor(Expression<Func<TEntity, bool>> predicate)
-    {
-        Predicate = predicate;
-    }
-
-}
-
-public class BoolOperationVisitor<TEntity> : APLGrammarBaseVisitor<Expression<Func<TEntity, bool>>> where TEntity : class
-{
-    private readonly Expression<Func<TEntity, bool>> Predicate;
-
-    public BoolOperationVisitor(Expression<Func<TEntity, bool>> predicate)
-    {
-        Predicate = predicate;
-    }
-
 }
